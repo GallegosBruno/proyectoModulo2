69861a2a32c95e2287ef6b626a9acdb05f1751a3
agusm <agusmena60@gmail.com>
2023-10-03 14:53:45 -0300

    boton cargar Peliculas

---
+++ javascript/detallePelicula.js
@@ -0,0 +1,30 @@
+let botonCargarPeliculas = document.getElementById("btnCargarPeliculas")
+console.log(botonCargarPeliculas)
+/* function cargarDatosPrueba() {
+    const datos = [
+      {
+        codigo: "01",
+        producto: "flor",
+        cantidad: "1",
+        descripcion: "flores",
+        url: "https://media.istockphoto.com/id/1362513839/es/foto/colorido-jard%C3%ADn.jpg?s=612x612&w=is&k=20&c=x02jBBREgbF8k9V0kPspToKAEZkQBsIXx88JH78EyY8=",
+      },
+      {
+        codigo: "04",
+        producto: "primavera",
+        cantidad: "1",
+        descripcion: "flores rosas",
+        url: "https://media.istockphoto.com/id/1362513839/es/foto/colorido-jard%C3%ADn.jpg?s=612x612&w=is&k=20&c=x02jBBREgbF8k9V0kPspToKAEZkQBsIXx88JH78EyY8=",
+      },
+    ];
+    if (!localStorage.getItem("arrayProductoKey")) {
+      //quiero agregar los datos de productos
+      console.log("cargar datos de prueba");
+      localStorage.setItem("arrayProductosKey", JSON.stringify(datos));
+      listaProductos = datos;
+      listaProductos.forEach((itemProducto) => {
+        crearFila(itemProducto);
+      });
+    }
+    
+  } */
