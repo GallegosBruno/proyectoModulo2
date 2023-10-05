a972b57301fe10cd7f1fddcee823c97ede6fa5cb
agusm <agusmena60@gmail.com>
2023-10-05 08:55:19 -0300

    cambios en detallesDePelicula

--- javascript/app.js
+++ javascript/app.js
@@ -9,154 +9,13 @@ const peliculas = [
         {
           codigo: "01",
           nombre: "Advengers",
-          porcentaje: "85%",
-          descripcion: "super héroes",
-          genero: "ficción",
-          destacado: true,
-          publicado: true,
-          url: "https://es.web.img3.acsta.net/c_310_420/pictures/17/01/25/13/52/576358.jpg"
-        },
-        {
-          codigo: "02",
-          nombre: "La Sirenita",
-          porcentaje: "85%",
-          descripcion: "Ariel, la sirenita",
-          genero: "infantil",
-          destacado: false,
-          publicado: false,
-          url: "https://es.web.img3.acsta.net/c_310_420/pictures/17/01/25/13/52/576358.jpg"
-        },
-        {
-          codigo: "03",
-          nombre: "El Señor de los Anillos",
-          porcentaje: "75%",
-          descripcion: "Épica aventura de fantasía",
-          genero: "fantasía",
-          destacado: true,
-          publicado: true,
-          url: "https://es.web.img3.acsta.net/c_310_420/pictures/17/01/25/13/52/576358.jpg"
-        },
-        {
-          codigo: "04",
-          nombre: "Jurassic Park",
-          porcentaje: "70%",
-          descripcion: "Dinosaurios regresan a la vida",
-          genero: "ciencia ficción",
-          destacado: false,
-          publicado: true,
-          url: "https://es.web.img3.acsta.net/c_310_420/pictures/17/01/25/13/52/576358.jpg"
-        },
-        {
-          codigo: "05",
-          nombre: "Harry Potter y la Piedra Filosofal",
-          porcentaje: "90%",
-          descripcion: "Magia y aventura en Hogwarts",
-          genero: "fantasía",
-          destacado: true,
-          publicado: true,
-          url: "https://es.web.img3.acsta.net/c_310_420/pictures/17/01/25/13/52/576358.jpg"
-        },
-        {
-          codigo: "06",
-          nombre: "Titanic",
-          porcentaje: "80%",
-          descripcion: "Amor y tragedia en alta mar",
-          genero: "drama",
-          destacado: false,
-          publicado: false,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "07",
-          nombre: "Star Wars: Una Nueva Esperanza",
-          porcentaje: "85%",
-          descripcion: "La lucha contra el Imperio Galáctico",
-          genero: "ciencia ficción",
-          destacado: true,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "08",
-          nombre: "El Padrino",
-          porcentaje: "90%",
-          descripcion: "La mafia en Nueva York",
-          genero: "drama",
-          destacado: false,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "09",
-          nombre: "E.T. el Extraterrestre",
-          porcentaje: "80%",
-          descripcion: "Amistad con un extraterrestre",
-          genero: "ciencia ficción",
-          destacado: true,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "10",
-          nombre: "Forrest Gump",
-          porcentaje: "85%",
-          descripcion: "La vida de Forrest Gump",
-          genero: "drama",
-          destacado: true,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "11",
-          nombre: "Matrix",
-          porcentaje: "80%",
-          descripcion: "Realidad virtual y rebelión",
-          genero: "ciencia ficción",
-          destacado: true,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "12",
-          nombre: "Toy Story",
-          porcentaje: "85%",
-          descripcion: "Aventuras de juguetes vivientes",
-          genero: "animación",
-          destacado: false,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "13",
-          nombre: "Piratas del Caribe: La Maldición del Perla Negra",
-          porcentaje: "75%",
-          descripcion: "Aventuras de piratas y maldiciones",
-          genero: "aventura",
-          destacado: true,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "14",
-          nombre: "Avatar",
-          porcentaje: "85%",
-          descripcion: "Exploración de Pandora",
-          genero: "ciencia ficción",
-          destacado: false,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-        {
-          codigo: "15",
-          nombre: "Los Increíbles",
-          porcentaje: "80%",
-          descripcion: "Superhéroes en familia",
-          genero: "animación",
-          destacado: true,
-          publicado: true,
-          url: "https://especiales.publico.es/resources/archivos/2016/1/13/1452680501600imagenesdelasinopsis.jpg"
-        },
-   
+          destacada: true,
+          srcImage: "https://es.web.img3.acsta.net/c_310_420/pictures/17/01/25/13/52/576358.jpg",
+          description: "super héroes",
+          categories: "ficción",
+          released: true,
+          trailerLink:"https://www.youtube.com/watch?v=PyakRSni-c0"
+        } 
     ]
  /*    if (!localStorage.getItem("arrayPeliculas")) {
       //quiero agregar los datos de productos
@@ -181,16 +40,16 @@ function crearCarrusel(){
     let carousel = document.getElementById("carrusel");
 
     for (let i = 0; i < listaPeliculas.length; i++) {
-        if (i <= 5 && listaPeliculas[i].publicado) {
+        if (i <= 5 && listaPeliculas[i].released) {
             carouselActivo.innerHTML+= `<a href="./pages/detallePelicula.html#${listaPeliculas[i].codigo}"><img
-            src=${listaPeliculas[i].url}
+            src=${listaPeliculas[i].srcImage}
             class="h-25 m-3"
             alt=${listaPeliculas[i].nombre}
           /></a>`
             
-        } else if(listaPeliculas[i].publicado) {
+        } else if(listaPeliculas[i].released) {
             carousel.innerHTML+= `<a href="./pages/detallePelicula.html#${listaPeliculas[i].codigo}"><img
-            src=${listaPeliculas[i].url}
+            src=${listaPeliculas[i].srcImage}
             class="img-fluid m-3"
             alt=${listaPeliculas[i].nombre}
           /></a>`
--- javascript/detallePelicula.js
+++ javascript/detallePelicula.js
@@ -1,17 +1,15 @@
 function getCodigoPeliculaFromHash() {
   const hash = location.hash;
-  console.log(hash)
+  console.log(hash);
   return hash.substring(1);
 }
 const codigoPelicula = getCodigoPeliculaFromHash();
 console.log("Código de la película:", codigoPelicula);
-let listaPeliculas = JSON.parse(localStorage.getItem("arrayPeliculas")) || []
-
-
+let listaPeliculas = JSON.parse(localStorage.getItem("arrayPeliculas")) || [];
 
 const detallePelicula = () => {
-  let detallePelicula = document.getElementById("detallePelicula")
-  let pelicula= listaPeliculas.find((item)=>item.codigo === codigoPelicula)
+  let detallePelicula = document.getElementById("detallePelicula");
+  let pelicula = listaPeliculas.find((item) => item.codigo === codigoPelicula);
 
   detallePelicula.innerHTML = `
 <h1 class="text-center display-3">Detalles de la pelicula</h1>
@@ -23,22 +21,23 @@ const detallePelicula = () => {
                     Original de <span class="fw-bold">RollingMovies+</span>
                   </p>
                   <p class="pt-4">
-                    <span class="text-success fw-bold">${pelicula.porcentaje} para ti</span> | 2016. 1hs
+                    <span class="text-success fw-bold">85% para ti</span> | 2016. 1hs
                     28 min
                   </p>
                   <p>
-                    ${pelicula.descripcion}
+                    ${pelicula.description}
                   </p>
-                  <button class="btn btn-reproducir px-4 mb-5 text-center"><i class="fa-solid fa-play" style="color: #ffffff;"></i> Reproducir</button>
+                  <button class="btn btn-reproducir px-4 mb-5 text-center"><i class="fa-solid fa-play" style="color: #ffffff;"></i><a href="${pelicula.trailerLink}" target="_blank"> Reproducir</a>
+                  </button>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6">
                   <div class="d-flex justify-content-center"> <img
-                    src=${pelicula.url}
+                    src=${pelicula.srcImage}
                     class="w-50"
                   /></div>
                  
                 </div>
-              </article>`
-}
+              </article>`;
+};
 
-detallePelicula()
+detallePelicula();
