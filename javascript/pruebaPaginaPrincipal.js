eac6125359920de19d24ce67d2b5f3a6ae708090
GallegosBruno <gallegosbrunom@gmail.com>
2023-10-05 16:36:54 -0300

    Arreglados errores menores

--- javascript/pruebaPaginaPrincipal.js
+++ javascript/pruebaPaginaPrincipal.js
@@ -1,46 +1,43 @@
 class Peliculas {
-  constructor(
-    codigo,
-    nombre,
-    destacada,
-    srcImage,
-    description,
-    categories,
-    released,
-    trailerLink,
-    
-  ) {
-    this.codigo = codigo;
-    this.nombre = nombre;
-    this.destacada = destacada;
-    this.poster = srcImage;
-    this.description = description;
-    this.categories = categories;
-    this.released = released;
-    this.link = trailerLink;
-  }
-
+    constructor(
+        codigo,
+        nombre,
+        destacada,
+        description,
+        srcImage,
+        categories,
+        released,
+        trailerLink)
+    {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.destacada = destacada;
+        this.poster = srcImage;
+        this.description = description;
+        this.categories = categories;
+        this.released = released;
+        this.link = trailerLink;
+    }
 }
 
-let peliculaDestacada = new peliculas(
-  "Transformes",
-  true,
-  "./assets/Posters de peliculas/pel5.jpg",
-  "Em la nuva de transforme todo muere pibe",
-  "Accion Aventura",
-  true,
-  "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+let peliculaDestacada = new Peliculas(
+    12356,
+    "Transformes",
+    true,
+    "Em la nuva de transforme todo muere pibe",
+    "./assets/Posters de peliculas/pel5.jpg",
+    "Accion aventura",
+    true,
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 );
 
 /*Funciones de pagina principal*/
 /*AÑADIR AL ARCHIVO DE JAVA COMUN Y BORRAR LO DE ARRIBA*/
 const setBanner = () => {
-  document.getElementById("mainBannerImg").src = peliculaDestacada.poster;
-  console.log(peliculaDestacada.poster);
-  document.getElementById("tituloBanner").innerText = peliculaDestacada.nombre;
-  document.getElementById("descripcionBanner").innerText =
-    peliculaDestacada.description;
-  document.getElementById("trailerLink").href = peliculaDestacada.link;
+    document.getElementById("mainBannerImg").src = peliculaDestacada.poster;
+    document.getElementById("tituloBanner").innerText = peliculaDestacada.nombre;
+    document.getElementById("descripcionBanner").innerText = peliculaDestacada.description;
+    document.getElementById("trailerLink").href = peliculaDestacada.link;
 };
 window.onload = () => {
   setBanner();
