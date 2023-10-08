72cd52f7b7621a300fb3ea39d5e562001dfc5681
Sofi Delgado <sofi5_09@hotmail.com>
2023-10-08 01:28:15 -0300

    Se crea js de about us donde se importan las validaciones

---
+++ javascript/aboutUs.js
@@ -0,0 +1,36 @@
+import {sumarioValidaciones,campoRequerido, validarMail} from "./helpers.js";
+
+let campoMail = document.getElementById("mail");
+let campoAsunto = document.getElementById("asunto");
+let campoDescripcion = document.getElementById("descripcion");
+let enviarMail = document.getElementById("formMail");
+const msjFormulario = document.getElementById('msjFormulario')
+
+campoMail.addEventListener("blur", () => {
+    console.log("desde mail");
+    if (validarMail(campoMail)) {
+      campoRequerido(campoMail, 5, 50);
+    } else {
+      campoMail.className = "form-control is-invalid";
+    }
+  });
+  campoAsunto.addEventListener("blur", () => {
+    console.log("desde asunto");
+    campoRequerido(campoAsunto, 5, 50);
+  });
+  campoDescripcion.addEventListener("blur", () => {
+    console.log("desde descripcion");
+    campoRequerido(campoDescripcion, 10, 1000);
+  });
+
+function enviarForm(e){
+    e.preventDefault();
+    let sumario = sumarioValidaciones(campoMail.value, campoAsunto.value, campoDescripcion.value);
+    if(sumario.length=== 0){
+        console.log('ENVIANDO FORM')
+    }
+    else{
+        msjFormulario.className = "alert alert-danger mt-3";
+        msjFormulario.innerHTML = sumario;
+    }
+}
---
+++ javascript/helpers.js
@@ -0,0 +1,58 @@
+export const campoRequerido = (input, min, max) => {
+  if (
+    input.value.trim().length > 0 &&
+    input.value.length <= max &&
+    input.value.length >= min
+  ) {
+    console.log("cumple cond")
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+};
+ 
+
+
+export const validarMail = (input) => {
+  const validacion = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  if (validacion.test(input.value) && input.value.trim().length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const validacionGral = (campoAsunto, campoDescripcion, campoMail) => {
+  if (
+    campoRequerido(campoMail) &&
+    campoRequerido(campoDescripcion) &&
+    campoRequerido(campoAsunto)
+  ) {
+  }
+};
+
+
+export function sumarioValidaciones (campoMail,campoAsunto,campoDescripcion){
+  let mensaje ='';
+  if(!campoRequerido(campoMail,5,50)){
+    console.log('campoMail')
+    mensaje += 'El mail debe contener entre 5 y 50 caracteres <br>';
+  }
+  if(!campoRequerido(campoAsunto,5,50)){
+    mensaje += 'El asunto debe contener entre 5 y 10 caracteres <br>';
+  }
+  if(!campoRequerido(campoDescripcion,10,1000)){
+    mensaje+='La descripcion debe contener entre 10 y 1000 caracteres <br>'
+  }
+  if(!validarMail(campoMail))
+  {
+    mensaje +='Debe completar el campo "correo electronico" <br> El mail debe contener @ y . seguido de com, ar etc'
+  }
+  if(mensaje.length!==0){
+    return mensaje;
+  }else{
+    return '';
+  }
+}
